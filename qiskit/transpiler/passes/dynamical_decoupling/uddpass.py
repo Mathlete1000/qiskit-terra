8bdd97bed92f9cb7b6026fc13e18224e6ae18f8e
Manning Chuor <manningc@mit.edu>
2020-07-27 13:14:40 -0400

    Used Delay.duration() instead of instruction_durations.get(), and implemented the rest of UDD, reformatting

--- qiskit/transpiler/passes/dynamical_decoupling/uddpass.py
+++ qiskit/transpiler/passes/dynamical_decoupling/uddpass.py
@@ -12,6 +12,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+"""UDD Pass"""
 from qiskit.circuit.library.standard_gates import XGate, YGate
 from qiskit.circuit.delay import Delay
 from qiskit.dagcircuit import DAGCircuit
@@ -20,8 +21,9 @@ from qiskit.transpiler.basepasses import TransformationPass
 from math import sin, pi
 
 class UDDPass(TransformationPass):
+    """UDD Pass"""
 
-    def __init__(self, N, backend_properties, dt_in_sec):
+    def __init__(self, N, backend_properties, dt_in_sec, tau_c=None):
         """UDDPass initializer.
         Args:
             N (int): Order of the UDD sequence to implement.
@@ -29,11 +31,14 @@ class UDDPass(TransformationPass):
                 backend, including information on gate errors, readout errors,
                 qubit coherence times, etc.
             dt_in_sec (float): Sample duration [sec] used for the conversion.
+            tau_c (int): Cycle time of the UDD sequence. Default is 2000 dt if
+                not specified.
         """
         super().__init__()
         self.N = N
         self.backend_properties = backend_properties
         self.dt = dt_in_sec
+        self.tau_c = 2000 if not tau_c else tau_c
 
     def run(self, dag):
         """Run the UDD pass on `dag`.
@@ -45,9 +50,8 @@ class UDDPass(TransformationPass):
             DAGCircuit: A new DAG with UDD Sequences inserted in large 
                         enough delays.
         """
-        tau_c = 2000      # In units of dt
         udd_durations = {}
-        tau_steps = {}
+        tau_steps_dict = {}
 
         u3_props = self.backend_properties._gates['u3']
         for qubit, props in u3_props.items():
@@ -55,16 +59,17 @@ class UDDPass(TransformationPass):
                 gate_length = props['gate_length'][0]
                 # TODO: Needs to check if total gate duration exceeds the input t_c
                 # If so, raise error
-                udd_durations[qubit[0]] = tau_c - int(self.N * round(gate_length / self.dt))
-                t_i = [int(round(udd_durations[qubit[0]] * (sin(pi * i / (2 * (self.N + 1)))) ** 2)) for i in range(self.N + 2)]
-                tau_steps[qubit[0]] = [t_i[i] - t_i[i-1] for i in range(1, self.N + 2)]
+                udd_durations[qubit[0]] = self.tau_c - int(self.N * round(gate_length / self.dt))
+
+                t_i = [int(round(udd_durations[qubit[0]] * \
+                                (sin(pi * i / (2 * (self.N + 1)))) ** 2)) \
+                                for i in range(self.N + 2)]
+
+                tau_steps_dict[qubit[0]] = [t_i[i] - t_i[i-1] for i in range(1, self.N + 2)]
                 # TODO: Maybe check if each tau step is 0, but makes error checking
                 # complicated in case of overflow of sum
                 # TODO: Also maybe check if sum(tau_steps) > udd_duration due to rounding up
                 # too frequently
-
-        for i in range(len(tau_steps)):
-            print(i, tau_steps[i])
 
         new_dag = DAGCircuit()
 
@@ -72,29 +77,29 @@ class UDDPass(TransformationPass):
             new_dag.add_qreg(qreg)
         for creg in dag.cregs.values():
             new_dag.add_creg(creg)
-
             
         for node in dag.topological_op_nodes():
-            print(dag.instruction_durations)
-            if isinstance(node.op, Delay):
-                delay_duration = dag.instruction_durations.get(node.op, node.qargs)
-                udd_duration = udd_durations[node.qargs[0].index]
-                print(udd_duration)
 
-                if tau_c <= delay_duration:
-                    count = int(delay_duration // tau_c)
-                    error = udd_duration - sum(tau_steps)             # Leftover from modulo errors
-                    parity = 1 if (delay_duration - count * tau_c + count * error) % 2 else 0
-                    new_delay = int((delay_duration - count * tau_c + count * error) / 2)
+            if isinstance(node.op, Delay):
+                delay_duration = node.op.duration
+                udd_duration = udd_durations[node.qargs[0].index]
+
+                if self.tau_c <= delay_duration:
+                    tau_steps = tau_steps_dict[node.qargs[0].index]
+                    count = int(delay_duration // self.tau_c)
+                    remainder = udd_duration - sum(tau_steps)
+                    parity = 1 if (delay_duration - count * self.tau_c + count * remainder) % 2 \
+                               else 0
+                    new_delay = int((delay_duration - count * self.tau_c + count * remainder) / 2)
 
                     new_dag.apply_operation_back(Delay(new_delay), qargs=node.qargs)
 
                     for _ in range(count):
-                        new_dag.apply_operation_back(Delay(udd_duration // 4), qargs=node.qargs)
-                        new_dag.apply_operation_back(YGate(), qargs=node.qargs)
-                        new_dag.apply_operation_back(Delay(udd_duration // 2), qargs=node.qargs)
-                        new_dag.apply_operation_back(YGate(), qargs=node.qargs)
-                        new_dag.apply_operation_back(Delay(udd_duration // 4), qargs=node.qargs)
+                        new_dag.apply_operation_back(Delay(tau_steps[0]), qargs=node.qargs)
+                        for tau_step in tau_steps[1:]:
+                            new_dag.apply_operation_back(YGate(), qargs=node.qargs)
+                            new_dag.apply_operation_back(Delay(tau_step), qargs=node.qargs)
+
 
                     new_dag.apply_operation_back(Delay(new_delay + parity), qargs=node.qargs)
 
