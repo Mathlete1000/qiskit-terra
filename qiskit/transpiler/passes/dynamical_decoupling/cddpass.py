5de6242c482429d6314c79d2a3b5893fa1af5aed
Manning Chuor <manningc@mit.edu>
2020-07-27 16:26:39 -0400

    Added/modified docstring, added optional cycle time parameter for CDD, fixed few calculation bugs

--- qiskit/transpiler/passes/dynamical_decoupling/cddpass.py
+++ qiskit/transpiler/passes/dynamical_decoupling/cddpass.py
@@ -12,14 +12,16 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+"""CDD Pass"""
 from qiskit.circuit.library.standard_gates import XGate, YGate
 from qiskit.circuit.delay import Delay
 from qiskit.dagcircuit import DAGCircuit
 from qiskit.transpiler.basepasses import TransformationPass
 
 class CDDPass(TransformationPass):
+    """CDD Pass"""
 
-    def __init__(self, N, backend_properties, dt_in_sec):
+    def __init__(self, N, backend_properties, dt_in_sec, tau_c=None, tau_step=10):
         """CDDPass initializer.
         Args:
             N (int): Order of the CDD sequence to implement.
@@ -27,11 +29,46 @@ class CDDPass(TransformationPass):
                 backend, including information on gate errors, readout errors,
                 qubit coherence times, etc.
             dt_in_sec (float): Sample duration [sec] used for the conversion.
+            tau_c (int): Cycle time of the DD sequence. Default is the sum of gate
+                durations of DD sequences with 10 ns delays in between.
+            tau_step (float): Delay time between pulses in the DD sequence. Default
+                is 10 ns.
         """
         super().__init__()
         self.N = N
         self.backend_properties = backend_properties
         self.dt = dt_in_sec
+        self.tau_step = tau_step
+        self.tau_cs = {}
+        self.tau_step_totals = {}
+        self.tau_c = tau_c
+
+        def build_sequence(N: int):
+            """
+            Recursively builds a list of gates to represent the CDD sequence
+            for the given order N (int).
+            """
+            if N == 1:
+                return [XGate(), YGate(), XGate(), YGate()]
+            return [XGate()] + build_sequence(N-1) + [YGate()] + build_sequence(N-1) + \
+                   [XGate()] + build_sequence(N-1) + [YGate()] + build_sequence(N-1)
+
+        self.cdd_sequence = build_sequence(self.N)
+
+        u3_props = self.backend_properties._gates['u3']
+        for qubit, props in u3_props.items():
+            if 'gate_length' in props:
+                gate_length = props['gate_length'][0]
+                # TODO: Needs to check if durations of gates exceed cycle time
+                # If so, raise error
+                if tau_c is None:
+                    self.tau_cs[qubit[0]] = len(self.cdd_sequence) * \
+                                        (round(tau_step * 1e-9 / self.dt + gate_length / self.dt))
+                    self.tau_step_totals[qubit[0]] = self.tau_cs[qubit[0]] - \
+                                            round(len(self.cdd_sequence) * gate_length / self.dt)
+                else:
+                    self.tau_step_totals[qubit[0]] = tau_c - \
+                                            round(len(self.cdd_sequence) * gate_length / self.dt)
 
     def run(self, dag):
         """Run the CDD pass on `dag`.
@@ -41,25 +78,6 @@ class CDDPass(TransformationPass):
             DAGCircuit: A new DAG with CDD Sequences inserted in large 
                         enough delays.
         """
-        def build_sequence(N):
-            if N == 1:
-                return [XGate(), YGate(), XGate(), YGate()]
-            return [XGate()] + build_sequence(N-1) + [YGate()] + build_sequence(N-1) + [XGate()] + build_sequence(N-1) + [YGate()] + build_sequence(N-1)
-
-        
-        cdd_durations = {}                        # TODO
-        cdd_sequence = build_sequence(self.N)
-        print("CDD", CDD_sequence)
-        tau_c = 5840
-
-        u3_props = self.backend_properties._gates['u3']
-        for qubit, props in u3_props.items():
-            if 'gate_length' in props:
-                gate_length = props['gate_length'][0]
-                # TODO: Needs to check if durations of gates exceed cycle time
-                # If so, raise error
-                cdd_durations[qubit[0]] = tau_c - int((4 ** self.N) * round(gate_length / self.dt))
-
         new_dag = DAGCircuit()
 
         for qreg in dag.qregs.values():
@@ -67,26 +85,26 @@ class CDDPass(TransformationPass):
         for creg in dag.cregs.values():
             new_dag.add_creg(creg)
 
-        print(dag.instruction_durations)
-
         for node in dag.topological_op_nodes():
 
             if isinstance(node.op, Delay):
-                delay_duration = dag.instruction_durations.get(node.op, node.qargs)
-                cdd_duration = cdd_durations[node.qargs[0].index]
+                delay_duration = node.op.duration
+                self.tau_step_total = self.tau_step_totals[node.qargs[0].index]
+                tau_c = self.tau_cs[node.qargs[0].index] if self.tau_c is None else self.tau_c
 
                 if tau_c <= delay_duration:
-                    count = int(delay_duration // tau_c)
-                    cdd_delay = cdd_duration // len(cdd_sequence)
-                    error = cdd_duration - len(cdd_sequence) * cdd_delay
-                    parity = 1 if (delay_duration - count * tau_c + error + cdd_delay) % 2 else 0
-                    new_delay = int((delay_duration - count * tau_c + error + cdd_delay) / 2)
+                    count = delay_duration // tau_c
+                    tau_step = self.tau_step_total // len(self.cdd_sequence)
+                    remainder = self.tau_step_total % len(self.cdd_sequence)
+                    parity = 1 if (delay_duration - count * (tau_c - remainder) + tau_step) % 2 \
+                               else 0
+                    new_delay = (delay_duration - count * (tau_c - remainder) + tau_step) // 2
 
-                    new_dag.apply_operation_back(Delay(new_delay - cdd_delay), qargs=node.qargs)
+                    new_dag.apply_operation_back(Delay(new_delay - tau_step), qargs=node.qargs)
 
                     for _ in range(count):
-                        for gate in CDD_sequence:
-                            new_dag.apply_operation_back(Delay(cdd_delay), qargs=node.qargs)
+                        for gate in self.cdd_sequence:
+                            new_dag.apply_operation_back(Delay(tau_step), qargs=node.qargs)
                             new_dag.apply_operation_back(gate, qargs=node.qargs)
 
                     new_dag.apply_operation_back(Delay(new_delay + parity), qargs=node.qargs)
